02cd20554c37ac5954e6a999ae6f8f686317ff26
Matias Klemola <matias.klemola@gmail.com>
2016-05-16 08:51:36 +0300

    use HTTPS connection for server, support upgrade request and accept any protocol

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 'use strict';
 
 const WebSocketServer = require('websocket').server;
-const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const crypto = require('crypto');
 
 const PORT = process.env.PORT || 5000;
 
-const server = http.createServer((request, response) => {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
-});
+function handler(request, response) {
+  console.log((new Date()) + ' Received request for ' + request.url);
+  response.writeHead(404);
+  response.end();
+}
+
+function upgrade(request, socket) {
+  const key = request.headers['sec-websocket-key'];
+  const hashedKey = crypto
+    .createHash('sha1')
+    .update(key+"258EAFA5-E914-47DA-95CA-C5AB0DC85B11")
+    .digest('base64');
+
+  const sResponse = "HTTP/1.1 101 Switching Protocols\r\n" +
+      "Upgrade: websocket\r\n" + "Connection: Upgrade\r\n" +
+      "Sec-WebSocket-Accept: " + key + "\r\n\r\n";
+      socket.write(sResponse,'ascii');
+}
+
+const httpsServer = https.createServer({
+  key: fs.readFileSync('cert/key.pem'),
+  cert: fs.readFileSync('cert/cert.pem'),
+}, handler);
 
 const wsServer = new WebSocketServer({
-    httpServer: server,
+    httpServer: httpsServer,
     autoAcceptConnections: false
 });
 
@@ -24,12 +44,13 @@ const greeting = {
 
 let messageHistory = [];
 
-server.listen(PORT, function() {
-    console.log('server up');
+httpsServer.onupgrade = upgrade;
+httpsServer.listen(PORT, function() {
+  console.log('HTTPS server up');
 });
 
 wsServer.on('request', (request) => {
-    const connection = request.accept('echo-protocol', request.origin);
+    const connection = request.accept(null, request.origin);
 
     console.log((new Date()) + ' Connection accepted.');
     connection.sendUTF(JSON.stringify(greeting));
